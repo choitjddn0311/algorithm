f43a14c3fde3a2a896f2891d87df1cd345f2812c
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-11-07 15:05:39 +0900

    Update 중앙값 구하기.js

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,5 +1,7 @@
 function solution(array) {
+    // sort함수는 순서대로 정렬함, 이같은 경우엔 a,b가 있고 a-b가 0보다 작으면 a가 앞 ,크면 b가 앞 같으면 그냥 놔둔다
     array.sort((a,b) => a-b);
+    // array의 length를 재서 나누기 2를 해준뒤 소숫점을 처리해주면 중앙값의 인덱스 번호가 나온다
     let answer = Math.floor(array.length/2);
     return array[answer];
     
