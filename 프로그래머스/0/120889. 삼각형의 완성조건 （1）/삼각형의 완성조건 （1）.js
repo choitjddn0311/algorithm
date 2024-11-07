a79e5eefd3f0b4a8bad2cfe0ce075ba84851f825
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-11-07 16:57:55 +0900

    Update 삼각형의 완성조건 （1）.js

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -3,10 +3,11 @@ function solution(sides) {
     const num = sides.sort((a,b)=> a-b);
     //array의 길이를 재서 인덱스번호의 맞춰 -1
     const last = sides.length-1;
-    //if문 야매로 해서 return
+    //if문으로 작은 두 변의 합이 가장 긴 변보다 작거나 같으면 return 2
     if(sides[last] >= sides[0] + sides[1]){
         return 2;
     } else {
+        //아니면 return 1
         return 1;
     }
 }
