3abc6c433fc4ad779eab8d70b5be8c37b63b9fbd
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-11-07 16:55:13 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -1,7 +1,7 @@
 function solution(sides) {
     const num = sides.sort((a,b)=> a-b);
-    const last = sides.length;
-    if(sides[last-1] >= sides[0] + sides[1]){
+    const last = sides.length-1;
+    if(sides[last] >= sides[0] + sides[1]){
         return 2;
     } else {
         return 1;
