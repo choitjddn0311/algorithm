8d1fe050da3150a41d9b6380000706489bdbff3c
최성우 <cswcjw311427@naver.com>
2025-11-12 23:52:29 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
+++ JavaScript/프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -1,11 +1,11 @@
 function solution(num) {
     // // 짝수니까 2로 나누었을때 나머지가 0이면 Even return
-    // if(num%2 === 0) {
+    // if(num%2 == 0) {
     //     return "Even"
     // } else {
     //     return "Odd"
     // }
     
     // 삼항연산자도 쌉가능
-    return num%2 === 0 ? "Even" : "Odd";
+    return num%2 == 0 ? "Even" : "Odd";
 }
