2299831d0098203efb24b5af1c034283a8aecfaa
최성우 <cswcjw311427@naver.com>
2025-11-16 01:27:29 +0900

    [level 0] Title: 공배수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181936. 공배수/공배수.js
+++ JavaScript/프로그래머스/0/181936. 공배수/공배수.js
@@ -1,7 +1,9 @@
 function solution(number, n, m) {
-    if(number%n == 0 && number%m == 0) {
-        return 1
-    } else {
-        return 0
-    }
+    // if(number%n == 0 && number%m == 0) {
+    //     return 1
+    // } else {
+    //     return 0
+    // }
+    
+    return number%n == 0 && number%m == 0 ? 1 : 0;
 }
