95a2bc80bb9f71bf8051fe6cc3cf692ce44eb1b3
최성우 <cswcjw311427@naver.com>
2025-11-21 09:30:19 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.js
+++ JavaScript/프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.js
@@ -1,3 +1,5 @@
 function solution(my_string, n) {
-    return my_string.split("").slice(0,n).join("");
+    // 굳이 자를 필요가 없었네
+    // return my_string.split("").slice(0,n).join("");
+    return my_string.slice(0,n);
 }
